88a56911ed0bddfde783e017ca7fe68e317b6f65
Mayank Yadav <xiatio08@gmail.com>
2021-10-09 21:13:33 +0530

    Adding initial files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCP2yxghmCyTT20gLFCQiJLsXWP5PCMWzI",
+    authDomain: "fir-421c1.firebaseapp.com",
+    projectId: "fir-421c1",
+    storageBucket: "fir-421c1.appspot.com",
+    messagingSenderId: "959638682801",
+    appId: "1:959638682801:web:b77b3659b777b292413690",
+    measurementId: "G-KV6YMFRYS8"
+  };
